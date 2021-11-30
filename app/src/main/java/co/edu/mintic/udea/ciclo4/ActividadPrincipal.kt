041a98eadfb7c8c70b4ece1242cd694f98ce9af8
difcTechnology <difc.technology@gmail.com>
2021-11-30 18:02:08 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/mintic/udea/ciclo4/ActividadPrincipal.kt
@@ -0,0 +1,11 @@
+package co.edu.mintic.udea.ciclo4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_actividad_principal)
+    }
+}
